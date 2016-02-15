71ef56e1acbb90d8b8dcef99d29991b891306dd9
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-02-15 22:47:47 +0200

    Create new tests for storage

--- test_storage.py
+++ test_storage.py
@@ -7,25 +7,31 @@ import pandas as pd
 class TestStorage(TestCase):
     def setUp(self):
         self.first = pd.Series([1, 3, 5, 'Nan', 6, 8])
-        self.second = pd.DataFrame({'A': 1.,
-                                    'B': pd.Timestamp('20130102'),
-                                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
-                                    'D': np.array([3] * 4, dtype='int32'),
-                                    'E': pd.Categorical(["test", "train", "test", "train"]),
-                                    'F': 'foo'})
-        self.third = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births'])
+        self.second = pd.DataFrame({'AAA' : [4,5,6,7], 'BBB' : [10,20,30,40],'CCC' : [100,50,-30,-50]})
+        self.third = pd.DataFrame(np.random.randn(6,1), index=pd.date_range('2013-08-01', periods=6, freq='B'), columns=list('A'))
 
-    def test_get(self):
-        storage = Storage()
-        storage.add('first', self.first)
-        print(storage.get('first') == self.first)
+    def test_get_first_when_none(self):
+        s = Storage('test')
+        print('After')
+        s.get('test')
+        print('Not Display')
+        with self.assertRaises(KeyError):
+            print('Before')
+            self.assertTrue(0 == 0)
 
-    def test2_get(self):
-        storage = Storage()
-        storage.add('second', self.second)
-        print(storage.get('second') == self.second)
+    def test_get_first(self):
+        s = Storage('test2')
+        s.add('first', self.first)
+        self.assertTrue(s.get('first').equals(self.first))
 
+    def test_get_two(self):
+        s = Storage('test2')
+        s.add('first', self.first)
+        s.add('first', self.second)
+        self.assertTrue(s.get('first').equals(self.second))
 
-names = ['Bob', 'Jessica', 'Mary', 'John', 'Mel']
-births = [968, 155, 77, 578, 973]
-BabyDataSet = list(zip(names, births))
+    def test_get_third(self):
+        s = Storage('test3')
+        s.add('third', self.third)
+        s.add('third2', self.third)
+        self.assertTrue(s.get('third').equals(s.get('third2')))
