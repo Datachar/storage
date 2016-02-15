e30126b7783fd4de942fa7822b13c24bbbec1cc1
Mykhailo Huzyuk <g.and.m@ukr.net>
2016-02-15 22:47:12 +0200

    New functionality

--- storage.py
+++ storage.py
@@ -1,32 +1,12 @@
 import pandas as pd
 
-index_name = 0
-index_size = 1
-index_df = 2
-
 
 class Storage:
-    def __init__(self):
-        self.storage = []
+    def __init__(self,name_file):
+        self.storage = pd.HDFStore(name_file + '.h5')
 
     def add(self, name, new_df):
-        if len(self.storage) != 0:
-            for i in self.storage:
-                if i[index_name] == name:
-                    new_input = input("\n DataFrame '" + name + "' is found. Enter new name - ")
-                    return self.add(new_input, new_df)
-        new_obj = list([])
-        new_obj.append(name)
-        new_obj.append(new_df.size)
-        new_obj.append(dict(new_df))
-        self.storage.append(new_obj)
+        self.storage[name] = new_df
 
     def get(self, name):
-        for i in self.storage:
-            if i[index_name] == name:
-                if i[index_size] > len(i[index_df]):
-                    return pd.DataFrame(i[index_df])
-                else:
-                    return pd.Series(i[index_df])
-        new_input = input("\n DataFrame '" + name + "' not found. Enter new name - ")
-        return self.get(new_input)
+        return self.storage[name]
